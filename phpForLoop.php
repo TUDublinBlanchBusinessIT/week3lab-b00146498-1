08cbd8eafbec7d8aed3de9c05e9599f435e00e9e
B00146498 Rebecca Mazilu <B00146498@mytudublin.ie>
2023-10-04 09:43:53 +0100

    got numbers divided by 5 on new line

--- phpForLoop.php
+++ phpForLoop.php
@@ -1,5 +1,5 @@
 <?php
-#phpForLoop - week3lab-b00146498
+#phpForLoop - week3lab-b00146498-1
 #rebecca mazilu 04/10/2023
 #this program uses a loop to count up to 20 and puts the value of the loop counter out to the screen on each iteration
 for ($i=1;$i<=20;$i++) {
